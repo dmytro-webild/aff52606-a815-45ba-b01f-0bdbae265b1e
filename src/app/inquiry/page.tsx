8e5dcc48036b4df9719dab24113082fcc7260033
bender <kudin.dima@gmail.com>
2026-04-14 19:15:27 +0000

    Update src/app/inquiry/page.tsx

--- src/app/inquiry/page.tsx
+++ src/app/inquiry/page.tsx
@@ -62,7 +62,7 @@ export default function InquiryPage() {
           <FooterSimple 
              columns={[{ title: "", items: [{ label: "Home", href: "/" }]}]} 
              bottomLeftText="Copyright 2026 Project Carbon Fiber" 
-             bottomRightText="hello@example.com"
+             bottomRightText="info@projectcarbonfiber.com"
           />
         </div>
       </ReactLenis>
