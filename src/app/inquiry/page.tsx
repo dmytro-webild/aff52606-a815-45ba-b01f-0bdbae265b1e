d3eb7287e26886cbc4a867ee884a64e996bc368e
bender <kudin.dima@gmail.com>
2026-04-14 19:47:10 +0000

    Update src/app/inquiry/page.tsx

--- src/app/inquiry/page.tsx
+++ src/app/inquiry/page.tsx
@@ -90,9 +90,9 @@ export default function InquiryPage() {
         </div>
         <div id="footer" data-section="footer">
           <FooterSimple 
-             columns={[{ title: "", items: [{ label: "Home", href: "/" }]}]}
-             bottomLeftText=""
-             bottomRightText=""
+             columns={[{ title: "Navigation", items: [{ label: "Home", href: "/" }]}]}
+             bottomLeftText="© 2024 Project Carbon Fiber"
+             bottomRightText="All Rights Reserved"
           />
         </div>
       </ReactLenis>
