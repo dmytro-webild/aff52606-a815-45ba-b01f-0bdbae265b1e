17865195585d9cc57096e279491f6b784ff2f7ac
bender <kudin.dima@gmail.com>
2026-04-14 19:15:28 +0000

    Update src/app/technology/page.tsx

--- src/app/technology/page.tsx
+++ src/app/technology/page.tsx
@@ -96,7 +96,7 @@ export default function LandingPage() {
         },
       ]}
       bottomLeftText="Copyright 2026 Project Carbon Fiber"
-      bottomRightText="hello@example.com"
+      bottomRightText="info@projectcarbonfiber.com"
     />
   </div>
       </ReactLenis>
